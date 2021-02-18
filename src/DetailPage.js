6abc6b695cfe4c0c4cdda20b18a6ac8d56e4f7c7
Brandon Perard <brandon.perard@gmail>
2021-02-17 22:04:43 -0800

    update functionality for details page route

--- src/DetailPage.js
+++ src/DetailPage.js
@@ -5,7 +5,9 @@ export default class DetailPage extends React.Component {
     render() {
         return (
             <div>
-
+                <h1>
+                    welcome to details
+                </h1>
             </div>
         )
     }
--- src/PokeItem.js
+++ src/PokeItem.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default class PokeItem extends React.Component {
     render() {
         return (
-            <Link to={this.props.pokemon.pokemon}>
+            <Link to={`/pokemon/${this.props.pokemon.pokemon}`}>
                 <div className="poke-div" key={this.props.pokemon._id}>
                     <h2>
                         {this.props.pokemon.pokemon}
