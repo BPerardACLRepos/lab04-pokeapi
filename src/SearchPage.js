47ad419df8e9a6bf7299f1db8606a9d2b3f6c473
Brandon Perard <brandon.perard@gmail>
2021-02-17 20:20:45 -0800

    add state to SearchPage.js

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -2,6 +2,15 @@ import React from 'react';
 import request from 'superagent';
 
 export default class SearchPage extends React.Component {
+
+    state = {
+        pokemonArray: [],
+        totalPokemon: 0,
+        pokemonPerPage: 20,
+        currentPage: 1,
+        loading: false,
+    }
+
     render() {
         return (
             <div>
