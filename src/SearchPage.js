24da02711f403cc977feb819a208e53f309ac729
Brandon Perard <brandon.perard@gmail>
2021-02-17 20:34:35 -0800

    add fetchPokemon() to SearchPage.js

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -11,6 +11,20 @@ export default class SearchPage extends React.Component {
         loading: false,
     }
 
+    fetchPokemon = async () => {
+
+        this.setState({ loading: true });
+
+        // we AWAITED a PROMISE
+        const pokeapiData = await request.get(`https://pokedex-alchemy.herokuapp.com/api/pokedex?page=${this.state.currentPage}&perPage=${this.state.pokemonPerPage}`);
+
+        this.setState({
+            loading: false,
+            pokemonArray: data.body.results,
+            totalPokemon: data.body.count
+        });
+    }
+
     render() {
         return (
             <div>
